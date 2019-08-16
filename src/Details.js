7ab11e0e3e38cb2bb096109c11c412ed33ba9a2c
KieranTheMan <kieran9fowler@gmail.com>
2019-08-16 15:03:43 +0100

    13# Introduce Component State

--- src/Details.js
+++ src/Details.js
@@ -1,8 +1,25 @@
 import React from 'react'
+class app extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+        message: `Hello, this will be the details page 
+        for each Movie & TV show :-)`
+        }
+    }
+componentDidMount(){ //waits for the componetent to mouted(initiated) and runs the code inside
+    setTimeout(()=> { this.setState({message:'Coming soon! :)'}); }, 3000);
 
-export default function Details() {
-    return (
-        <h1>Hello, this will be the details page for each Movie & TV show</h1>
-    );
 }
+
+
+render() {
+    return ( 
+        <div>
+            <h1>{this.state.message}</h1>
+        </div>
+    );
+  }
+}
+export default app
 
