c66539d99b4866a398181df9f3238c5b812c68cf
KieranTheMan <kieran9fowler@gmail.com>
2019-08-29 20:21:56 +0100

    #19 Return a list of shows from the backend

--- src/BackEnd/App.js
+++ src/BackEnd/App.js
@@ -1,7 +1,10 @@
 const express = require('express');
 const app = express();
 const port = 3001;
+const shows = require('./shows');
 
-app.get('/', (req, res) => res.send('Hello World!'));
+app.get('/rest/shows', (req, res) => res.send(shows()));
+app.get('/', (req, res) => res.send('this is our server root'));
+
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
---
+++ src/BackEnd/shows.js
@@ -0,0 +1,42 @@
+function MoviesGet() {
+    return [
+        {
+            id:'Afro',
+            name:'AFRO SAMURAI', 
+            alt:'AFRO', 
+            synopsis:`After obtaining the Number One headband, which proclaims him to be the best of all warriors, Afro spends most of his days in peace, though his nightmare-filled nights are not so tranquil. His life crescendos into chaos once again when Jinno and Sio, his former friends from long ago, take the headband from him, as well as the remains of his father. Challenged by Sio to don the Number Two headband again and exact revenge on them for their actions, Afro embarks on another journey as the wearer of said headband—an item for which anyone would kill him. Afro Samurai: Resurrection follows Afro as he fights a second series of battles against all manner of foes, wading through the sea of corpses in his wake so that he may once again quench his never-ending thirst for revenge.`
+                },
+        {
+            id:'Akira', 
+            name:'AKIRA', 
+            alt:'AKIRA',
+            synopsis:`In 1988 the Japanese government drops an atomic bomb on Tokyo after ESP experiments on children go awry. In 2019, 31 years after the nuking of the city, Kaneda, a bike gang leader, tries to save his friend Tetsuo from a secret government project. He battles anti-government activists, greedy politicians, irresponsible scientists and a powerful military leader until Tetsuo's supernatural powers suddenly manifest. A final battle is fought in Tokyo Olympiad exposing the experiment's secrets.`
+                },
+        {
+            id:'Dragon', 
+            name:"DRAGON BALL Z", 
+            alt:"DRAGON BALL Z",
+            synopsis:`One peaceful day on Earth, two remnants of Frieza's army named Sorbet and Tagoma arrive searching for the Dragon Balls with the aim of reviving Frieza. They succeed, and Frieza subsequently seeks revenge on the Saiyans.`
+                },       
+        {
+            id:'One',
+            name:'ONE PIECE',
+            alt:'OnePFilm',
+            synopsis:`The first chapter of the One Piece manga hit store shelves on Aug. 24, 1997 in the weekly Shōnen Jump magazine. It took less than two years for One Piece to become a full-fledged anime series with the first episode airing on Oct. 20, 1999. Since then, One Piece has grown to be the most popular manga series of all time in Japan with over 280 million volumes sold as of 2012. One Piece follows the story of a pirate named Monkey D. Luffy and his rag-tag Straw Hat pirate crew as they set-out to help Luffy become The King of the Pirates by finding a legendary treasure called One Piece that the last Pirate King Gold Roger hid at the end of the Grand Line, a series of islands separated from the rest of the world by windless oceans that cannot be sailed. While sometimes bloody, One Piece is generally good for the whole family and has such a compelling story that you’ll likely want to start reading the manga once you’ve seen any of the anime series or movies.`
+                },
+        {
+            id:'Spirited',
+            name:'SPIRITED AWAY',
+            alt:'SPIRITED AWAY',
+            synopsis: `In this animated feature by noted Japanese director Hayao Miyazaki, 10-year-old Chihiro (Rumi Hiiragi) and her parents (Takashi Naitô, Yasuko Sawaguchi) stumble upon a seemingly abandoned amusement park. After her mother and father are turned into giant pigs, Chihiro meets the mysterious Haku (Miyu Irino), who explains that the park is a resort for supernatural beings who need a break from their time spent in the earthly realm, and that she must work there to free herself and her parents.`
+            },
+        {
+            id:'Ghost',
+            name:'GHOST IN THE SHELL',
+            alt:'GHOST IN SHELL',
+            synopsis: `In this Japanese animation, cyborg federal agent Maj. Motoko Kusanagi (Mimi Woods) trails "The Puppet Master" (Abe Lasser), who illegally hacks into the computerized minds of cyborg-human hybrids. Her pursuit of a man who can modify the identity of strangers leaves Motoko pondering her own makeup and what life might be like if she had more human traits. With her partner (Richard George), she corners the hacker, but her curiosity about her identity sends the case in an unforeseen direction.`
+        }
+    ];
+}
+
+module.exports = MoviesGet
