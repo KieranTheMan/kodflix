697c672bd48da24001292a1ceb449dd9b635dc48
KieranTheMan <kieran9fowler@gmail.com>
2019-09-03 20:50:03 +0100

    23# updated,fixed App.js backendmethods,and name errors

--- src/BackEnd/App.js
+++ src/BackEnd/App.js
@@ -5,11 +5,8 @@ const shows = require('./shows');
 const path = require('path')
 
 app.get('/rest/shows', (req, res) => res.send(shows()));
-app.get('/', (req, res) => res.send('this is our server root'));
-
-app.get('*', function (req, res) {
-    res.sendfile(path.join(__dirname,'../../build'));
+app.use(express.static(path.join(__dirname, '../../build')));
+app.get('/', function (req, res) {
+    res.sendFile(path.join(__dirname,'../../build','index.html'));
 })
-app.use(express.static(path.join(__dirname, '../../build', 'index.html')));
-
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
