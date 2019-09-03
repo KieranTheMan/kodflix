552399f51e7a98786dc82d005155a88f0a8fdd3c
KieranTheMan <kieran9fowler@gmail.com>
2019-09-03 18:50:54 +0100

    22# connect front-end and back-end servers

--- src/BackEnd/App.js
+++ src/BackEnd/App.js
@@ -6,5 +6,4 @@ const shows = require('./shows');
 app.get('/rest/shows', (req, res) => res.send(shows()));
 app.get('/', (req, res) => res.send('this is our server root'));
 
-
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
--- src/FrontEnd/AllMovies.js
+++ src/FrontEnd/AllMovies.js
@@ -1,15 +1,44 @@
 import React from "react";
-import MovieItems from './MoviesComp';
-import MoviesGet from './MoviesGet';
+import MovieItems from "./MoviesComp";
+import MoviesGet from "./MoviesGet";
 
-export default function AllMovies() {
+export default class AllMovies extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      shows: [],
+      error: null
+    };
+  }
+
+  fetchMovies() {
+    fetch("rest/shows")
+      .then(res => res.json())
+      .then(data => this.setState({ shows: data }))
+      .catch(error => this.setState({ error }));
+  }
+
+  componentDidMount() {
+    this.fetchMovies();
+  }
+  render() {
     return (
-        <div>
-            <div className='Movies'>{
-                MoviesGet().map(movie => {
-                    return <MovieItems key={movie.id} id={movie.id} name={movie.name} img={movie.img} alt={movie.alt}/>
-                })
-            }</div>
+      <div>
+        <div className="Movies">
+          {" "}
+          {MoviesGet().map(movie => {
+            return (
+              <MovieItems
+                key={movie.id}
+                id={movie.id}
+                name={movie.name}
+                img={movie.img}
+                alt={movie.alt}
+              />
+            );
+          })}
         </div>
+      </div>
     );
+  }
 }
