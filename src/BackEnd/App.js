39eaa7a0137275a6ff2e4d2c0d18ed1f31e0c206
KieranTheMan <kieran9fowler@gmail.com>
2019-09-04 12:33:44 +0100

    made correct start script for heroku deployment

--- src/BackEnd/App.js
+++ src/BackEnd/App.js
@@ -7,6 +7,6 @@ const path = require("path");
 app.get("/rest/shows", (req, res) => res.send(shows()));
 app.use(express.static(path.join(__dirname, "../../build")));
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
-app.get("/", function(req, res) {
+app.get("*", function(req, res) {
   res.sendFile(path.join(__dirname, "../../build", "index.html"));
 });
